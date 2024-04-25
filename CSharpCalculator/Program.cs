8d149b84169da2ae6d36e936b39cc3ce89e05b67
Ulysses Andulte <117939069+yulz008@users.noreply.github.com>
2024-04-25 15:58:16 +0800

    Initial Code

---
+++ CSharpCalculator/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+// Simple calculator class
+public class Calculator
+{
+    // Addition operation
+    public double Add(double a, double b)
+    {
+        // Addition Operation
+        return a + b;
+    }
+
+    // Subtraction operation
+    public double Subtract(double a, double b)
+    {
+        // Subtraction Operation
+        return a - b;
+    }
+
+    // Multiplication operation
+    public double Multiply(double a, double b)
+    {
+        // Multiplication Operation
+        return a * b;
+    }
+
+    // Division operation
+    public double Divide(double a, double b)
+    {
+        // Division Operation
+        if (b == 0)
+        {
+            throw new ArgumentException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+}
+
+// Main application code
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+
+        if (args.Length < 3)
+        {
+            Console.WriteLine("Usage: dotnet run <operation> <num1> <num2>");
+            Console.WriteLine("Operations: add, subtract, multiply, divide");
+            return;
+        }
+
+        string operation = args[0];
+        double a = double.Parse(args[1]);
+        double b = double.Parse(args[2]);
+
+        switch (operation)
+        {
+            case "add":
+                Console.WriteLine("Addition: " + calc.Add(a, b));
+                break;
+            case "subtract":
+                Console.WriteLine("Subtraction: " + calc.Subtract(a, b));
+                break;
+            case "multiply":
+                Console.WriteLine("Multiplication: " + calc.Multiply(a, b));
+                break;
+            case "divide":
+                try
+                {
+                    Console.WriteLine("Division: " + calc.Divide(a, b));
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid operation. Use add, subtract, multiply, or divide.");
+                break;
+        }
+    }
+}
---
+++ CShartCalculator Tests/Tests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass()]
+    public class Tests
+    { 
+        [TestMethod()]
+        public void TestAdd()
+        {
+            Calculator calculator = new Calculator();
+           
+            double a = 5;
+            double b = 10;
+
+            double result = calculator.Add(a, b);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(15, result);   
+        }
+
+        [TestMethod()]
+        public void TestSubtract()
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 10;
+            double b = 5;
+
+            double result = calculator.Subtract(a, b);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(5, result);
+        }
+
+        [TestMethod()]
+        public void TestMultiply()
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 10;
+            double b = 5;
+
+            double result = calculator.Multiply(a, b);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(50, result);
+        }
+
+        [TestMethod()]
+        public void TestDivide()
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 10;
+            double b = 5;
+
+            double result = calculator.Divide(a, b);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void TestDivideByZero()
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 10;
+            double b = 0;
+
+            try
+            {
+                double result = calculator.Divide(a, b);
+            }
+            catch (ArgumentException e)
+            {
+                Microsoft.VisualStudio.TestTools.UnitTesting.StringAssert.Contains(e.Message,"Division by zero is not allowed");
+                return;
+            }
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.Fail("No Exeption was thrown");
+        }
+    }
+}
